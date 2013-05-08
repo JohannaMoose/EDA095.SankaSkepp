8a97d88bd6d5bddf350e7f5c31d4b40efc3e5023
Alexander Ekdahl <ekdahlsandor@gmail.com>
2013-05-08 10:55:26 +0200

    Skeleton for my classes

---
+++ Sanka Skepp/src/theGame/GameHandler.java
@@ -0,0 +1,21 @@
+
+public class GameHandler {
+	
+	private Board board;
+	private Player p1;
+	private Player p2;
+	
+	
+	public GameHandler(Player p1, Player p2, Board board){
+		
+	}
+	
+	public boolean isReady(){
+		if(p1.isReady() && p2.isReady()){
+			return true;
+		}
+		return false;
+	}
+	
+
+}
