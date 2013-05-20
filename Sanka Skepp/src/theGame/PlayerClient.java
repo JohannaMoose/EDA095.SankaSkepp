d577c561cc7f74ed78d72c4a48dc4152150c26b3
Alexander Ekdahl <ekdahlsandor@gmail.com>
2013-05-20 14:20:25 +0200

    playerClient klassen lite grann

--- Sanka Skepp/src/theGame/PlayerClient.java
+++ Sanka Skepp/src/theGame/PlayerClient.java
@@ -1,34 +1,68 @@
 package theGame;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 
 public class PlayerClient {
 	private String name;
+	
 	private Socket socket;
 	private boolean isReady=false;
-	
-	public PlayerClient(Socket s){
+	private String serverName;
+	private int portNbr;
+
+	public PlayerClient(Socket s, String serverName, int portNbr, String name){
 		this.socket=s;
-		
+		this.name=name;
+		this.serverName = serverName;
+		this.portNbr=portNbr;
+		try
+		{
+			System.out.println("Connecting to " + serverName
+					+ " on port " + portNbr);
+			Socket client = new Socket(serverName, portNbr);
+			System.out.println("Just connected to "
+					+ client.getRemoteSocketAddress());
+			OutputStream outToServer = client.getOutputStream();
+			DataOutputStream out =
+					new DataOutputStream(outToServer);
+
+			out.writeUTF("Hello from "
+					+ client.getLocalSocketAddress());
+			InputStream inFromServer = client.getInputStream();
+			DataInputStream in =
+					new DataInputStream(inFromServer);
+			System.out.println("Server says " + in.readUTF());
+			client.close();
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
-	
+
+
+
 	public boolean isReady(){
 		return isReady;
-		
+
 	}
-	
+
 	public void setName(String name){
 		this.name=name;
 	}
-	
+
 	public String getName(){
 		return name;
 	}
-	
-	
+
+
 	//Todo
 	public String getEnemyName(){
 		return name;
 	}
-	
+
 }
